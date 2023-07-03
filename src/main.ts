155a1b7b4acadb10974f565a52cd9d3c0d003908
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-07-03 13:45:14 +0900

    fix: get port from environment variable

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   const port = 3000;
-  await app.listen(port);
+  await app.listen(process.env.PORT || port);
   console.log(`Listening on port http://localhost:${port}`);
 }
 bootstrap();
